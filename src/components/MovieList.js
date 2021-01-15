5f2fa74bb4ddc427acfcaa1e3dd2f69bc9e9eb6c
Corey Burkett <coreyburkett22@gmail.com>
2021-01-14 20:58:29 -0800

    Added movie fetch request with OMDb api key

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,11 +4,13 @@ const MovieList = (props) => {
 
 
     return (
-        <div>
-            {props.movies.map((movie, index) => <div>
-                <img src={movie.Poster} alt="movie"></img>
-            </div>)}
-        </div>
+        <>
+            {props.movies.map((movie, index) => (
+                <div className="d-flex justify-content-start m-3">
+                    <img src={movie.Poster} alt="movie"></img>
+                </div>
+            ))}
+        </>
     )
 }
 
