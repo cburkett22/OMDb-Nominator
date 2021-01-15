36a17aa72ccbd73c0cad89a93b357b3de778c731
Corey Burkett <coreyburkett22@gmail.com>
2021-01-15 07:24:44 -0800

    Updated MovieList CSS/scrollbar/added title & year

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -8,6 +8,7 @@ const MovieList = (props) => {
         <>
             {props.movies.map((movie, index) => (
                 <div className='image-container d-flex justify-content-start m-3'>
+                    <span>{ movie.Title }: ({ movie.Year })</span>
                     <img src={ movie.Poster } alt='movie'></img>
                     <div onClick={ () => props.handleNominateClick(movie) } className='overlay d-flex align-items-center justify-content-center'>
                         <NominateComponent />
